d5cad25e9246fe952282f7c7eb32101e8e27a7de
Choco0914 <heojeongho1992@gmail.com>
2018-10-31 13:44:24 +0900

    Add url, view, template

--- week-02-django/src/blog/views.py
+++ week-02-django/src/blog/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
+# from random import randint
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    """index는 사이트의 문의 처럼 대문역할을 해준다"""
+    # random_number = randint(1, 10)
+    # return HttpResponse("Hello, world! {}".format(random_number))
+    # return HttpResponse("Hello, world!")
+    name = "choco"
+    context = {'name':name,}
+    return render(request, "blog/index.html", context)
