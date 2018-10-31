4432b229425ca96f18fbd5a366b47b5ce6759fc3
Choco0914 <heojeongho1992@gmail.com>
2018-10-31 12:13:40 +0900

    Add week-02-django project
    - started blog app

---
+++ week-02-django/django/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/c/Users/heoje/Desktop/linked/coding-for-beginner/week-02-django/django/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
