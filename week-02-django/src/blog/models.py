f549c86660ce297b67a41c7a4fb0790e4ecf8bee
Choco0914 <heojeongho1992@gmail.com>
2018-11-01 18:02:02 +0900

    feet: Learn model and Sql
    	- Eddited settings.py
    	- Add class article, comment in models.py
    	- Did makemigration, migrate
    	- Created Article objects

--- week-02-django/src/blog/models.py
+++ week-02-django/src/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Create Your models here.
+class Article(models.Model):
+    """게시글의 모델을 정의해준다."""
+    title = models.CharField(max_length=30)
+    contents = models.TextField()
+    view_count = models.IntegerField()
+
+class Comment(models.Model):
+    """게시글의 댓글 모델을 정의해준다."""
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=100)
--- week-02-django/src/blog/views.py
+++ week-02-django/src/blog/views.py
@@ -1,12 +1,12 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
 # from random import randint
-
+from . models import Article
 def index(request):
     """index는 사이트의 문의 처럼 대문역할을 해준다"""
     # random_number = randint(1, 10)
     # return HttpResponse("Hello, world! {}".format(random_number))
     # return HttpResponse("Hello, world!")
-    name = "choco"
-    context = {'name':name,}
+    article_list = Article.objects.all()
+    context = {'article_list':article_list,}
     return render(request, "blog/index.html", context)
