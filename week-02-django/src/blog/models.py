91139da1521c0319d51da5a9a791541b1e954f1e
Choco0914 <heojeongho1992@gmail.com>
2018-11-02 12:28:28 +0900

    feet: Using Admin, Bootstrap, font awesome
    	- Eddited admin.py
    	- Download Bootstrap, Font Awesome
    	- Using Bootstrap and font awesome to styling our web site

--- week-02-django/src/blog/models.py
+++ week-02-django/src/blog/models.py
@@ -7,6 +7,10 @@ class Article(models.Model):
     contents = models.TextField()
     view_count = models.IntegerField()
 
+    def __str__(self):
+        """등록되어 있는 모델의 객체를 표시해준다"""
+        return '%s %s' % (self.title, self.view_count)
+
 class Comment(models.Model):
     """게시글의 댓글 모델을 정의해준다."""
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
